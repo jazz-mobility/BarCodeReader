7ab47fefb6e60827ef9553dab85311bba7bec6a1
Jasveer Singh <Jasveer@JazZ.local>
2012-09-06 20:03:25 +0530

    Initial Commit

---
+++ BarCodeReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BarCodeReader
+//
+//  Created by Jasveer Singh on 06/09/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
